b6ab69b4fa665c89530191d537bacd49d5c10b19
cungen <1360571582@qq.com>
2020-08-19 23:51:30 +0800

    init commit

---
+++ douguo_spider/items.py
@@ -0,0 +1,26 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class DouguoSpiderItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+
+    # title = scrapy.Field()
+    # img = scrapy.Field()
+    # author_img = scrapy.Field()
+    # author = scrapy.Field()
+    # pv = scrapy.Field()
+    # stars = scrapy.Field()
+    # intro = scrapy.Field()
+    # materials = scrapy.Field()
+    # materials_quantity = scrapy.Field()
+    # steps = scrapy.Field()
+    # tips = scrapy.Field()
+    # category = scrapy.Field()
+
+    pass
